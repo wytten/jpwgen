1852e28aa1ef98660c4d36b97a659b1647e1207e
Dale Wyttenbach <dwyttenbach@decare.com>
2020-06-08 16:39:12 -0500

    Fixed bug in run.sh

--- jpwgen/run.sh
+++ jpwgen/run.sh
@@ -3,4 +3,4 @@ n=32
 if [ $# -gt 0 ]; then
     n=$1
 fi
-java -jar target/jpwgen-0.0.1-SNAPSHOT.jar --digits 12 --lowercase --uppercase -n $n
+java -jar target/jpwgen-0.0.1-SNAPSHOT.jar --digits --lowercase --uppercase --length 12 -n $n
